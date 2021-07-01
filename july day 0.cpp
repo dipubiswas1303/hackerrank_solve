be1f8296f0e58233771dd8e6acd3c9486158ef13
Dipu Biswas <dipubiswas1303@gmail.com>
2021-07-01 17:24:19 +0600

    Day 0: Hello, World.
    https://www.hackerrank.com/challenges/30-hello-world/problem

---
+++ july day 0.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*******************
+NAME : DIPU BISWAS
+JUST CSE 2019 - 2020
+PROBLEM CODE : Day 0: Hello, World.
+LINK : https://www.hackerrank.com/challenges/30-hello-world/problem
+*******************/
+
+int main()
+{
+    string a;
+    getline(cin, a);
+    cout << "Hello, World." << endl;
+    cout << a << endl;
+    return 0;
+}
+
