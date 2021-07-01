1ee7667c60dd2bd7b6c82b9062ce42d3460a4320
Dipu Biswas <dipubiswas1303@gmail.com>
2021-07-01 18:18:22 +0600

    For Loop
     https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem

--- For Loop.cpp
+++ For Loop.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 
+using namespace std;#include <bits/stdc++.h>
+
 using namespace std;
 /*******************
 NAME : DIPU BISWAS
 JUST CSE 2019 - 2020
 PROBLEM CODE : For Loop
-
-
 LINK : https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem
 *******************/
 
@@ -26,6 +26,31 @@ int main()
             cout << "odd" << endl;
         }
     }
+    return 0;
+}
+
+
+/*******************
+NAME : DIPU BISWAS
+JUST CSE 2019 - 2020
+PROBLEM CODE : Functions
+LINK : https://www.hackerrank.com/challenges/c-tutorial-functions/problem?h_r=next-challenge&h_v=zen
+*******************/
+int ret(int a, int b, int c, int d){
+    if(a >= b && a >= c && a >= d)
+        return a;
+    else if(b >= a && b >= c && b >= d)
+        return b;
+    else if(c >= a && c >= b && c >= d)
+        return c;
+    else
+        return d;
+}
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << ret(a, b, c, d) << endl;
     return 0;
 }
 
